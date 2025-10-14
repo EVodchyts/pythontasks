c7a00a6a67d0dd16969bdaf80399277debd8c171
Евгений Водчиц <eugenevodchits@gmail.com>
2025-10-14 14:22:58 +0300

    Added my solution for LeetCode Plus One

---
+++ plusone.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        result = []
+        reminder = 1
+
+        for i in digits[::-1]:
+            total = i + reminder
+            result.append(total % 10)
+            reminder = total // 10
+        return result[::-1]
+    
+def main():
+    test = Solution()
+    print(test.plusOne([1,2,9]))
+    print(test.plusOne([1,2,9,9,9]))
+
+
+if __name__ == "__main__":
+    main()
