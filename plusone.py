0585facc7d26c5f2cf87cc9764d2e2f7e88b4162
Евгений Водчиц <eugenevodchits@gmail.com>
2025-10-14 14:24:37 +0300

    Fixed Plus One after tests

--- plusone.py
+++ plusone.py
@@ -3,18 +3,23 @@ from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         result = []
-        reminder = 1
+        carry = 1  # додаємо 1 до останнього елемента
 
         for i in digits[::-1]:
-            total = i + reminder
+            total = i + carry
             result.append(total % 10)
-            reminder = total // 10
+            carry = total // 10  # 1 якщо було 10 або більше, інакше 0
+
+        if carry:
+            result.append(carry)
+
         return result[::-1]
     
 def main():
     test = Solution()
-    print(test.plusOne([1,2,9]))
-    print(test.plusOne([1,2,9,9,9]))
+    #print(test.plusOne([1,2,9]))
+    #print(test.plusOne([1,2,9,9,9]))
+    print(test.plusOne([9]))
 
 
 if __name__ == "__main__":
